6ad4e1952cd96687c002a632b14efddd62ed5acc
youht <youht@youhtdeMac-mini.local>
2024-12-02 12:13:58 +0800

    add flow

--- yyc_stock/stock/akshare/__init__.py
+++ yyc_stock/stock/akshare/__init__.py
@@ -207,7 +207,7 @@ class AkshareStock(StockBase):
     def minute_pro(self):
         minute_pro_db=sqlite3.connect("minute_pro.db")
         codes_info = self._get_bk_codes("hs_a")
-        codes_info = codes_info[:100]
+        #codes_info = codes_info[:100]
         #codes_info=[{'dm':'300159','mc':'新研股份'}]
         print("total codes:",len(codes_info))
         with tqdm(total=len(codes_info),desc="进度") as pbar:
@@ -827,7 +827,42 @@ class AkshareStock(StockBase):
                 return HTMLResponse(content=content)
             except Exception as e:
                 raise HTTPException(status_code=400, detail=f"{e}")
-        
+        @self.router.get("/flow/rank/{type}/{period}")
+        async def flow_rank_gg(type:str,period:str,req:Request):
+            """获取当前资金流数据"""
+            try:
+                #sector_type: choice of {"行业资金流", "概念资金流", "地域资金流"}
+                types={"gg":"individual","hy":"行业资金流","gn":"概念资金流","dy":"地域资金流"}
+                periods={"0":"今日","3":"3日","5":"5日","10":"10日"}
+                func=None
+                period_str=periods.get(period)
+                if not period_str and type=='gg':
+                    raise Exception("period must be 0,3,5,10")
+                if period_str=='3日' and type!='gg':
+                    raise Exception("period must be 0,5,10")
+                
+                type_str = types.get(type)
+                print(type_str,period_str)
+                if not type_str:
+                    raise Exception("type must be 'gg' or 'hy' or 'gn' or 'dy'")
+                if type=="gg":
+                    df = ak.stock_individual_fund_flow_rank(indicator=period_str)
+                else:
+                    df = ak.stock_sector_fund_flow_rank(indicator=period_str,sector_type=type_str)
+                df = df[df.最新价!='-']
+                df = df.rename(columns={"代码":"dm","名称":"mc","最新价":"zxj",f"{period_str}涨跌幅":f"zd{period}",
+                                        f"{period_str}主力净流入-净额":f"zljlr{period}",f"{period_str}主力净流入-净占比":f"zljlrl{period}",
+                                        f"{period_str}超大单净流入-净额":f"cddjlr{period}",f"{period_str}超大单净流入-净占比":f"cddjlrl{period}",
+                                        f"{period_str}大单净流入-净额":f"ddjlr{period}",f"{period_str}大单净流入-净占比":f"ddjlrl{period}",
+                                        f"{period_str}中单净流入-净额":f"zdjlr{period}",f"{period_str}中单净流入-净占比":f"zdjlrl{period}",
+                                        f"{period_str}小单净流入-净额":f"xdjlr{period}",f"{period_str}小单净流入-净占比":f"xdjlrl{period}",
+                                        })
+                df.pop("序号")
+                df = self._prepare_df(df,req)
+                content = self._to_html(df)
+                return HTMLResponse(content=content)
+            except Exception as e:
+                raise HTTPException(status_code=400, detail=f"{e}")
         @self.router.get("/current")
         async def current(req:Request):
             """获取当前行情数据"""
